cab19df03a5eef93515788e25ba76caad1a44d63
DahmaniDev <mohamedhedi.dahmani@gmail.com>
2021-02-28 20:34:45 +0100

    First commit

---
+++ src/controllers/AccueilController.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import entities.User;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author Dahmani
+ */
+public class AccueilController implements Initializable {
+
+    @FXML
+    private Button accueilBtn;
+    @FXML
+    private Button profilBtn;
+    @FXML
+    private Button formationsBtn;
+    @FXML
+    private Button test_quizBtn;
+    @FXML
+    private Button chatBtn;
+    @FXML
+    private Button deconnectBtn;
+    @FXML
+    private Label welcomeLabel;
+    
+    User currentUser;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+         
+    }
+
+    public void initData(User u){
+        this.currentUser=u;
+        welcomeLabel.setText("Bienvenue "+currentUser.getNom());
+    }
+
+    @FXML
+    private void handleAccueilBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void HandleAccueilBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleTestQuizBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleChatBtn(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleDeconnectBtn(ActionEvent event) {
+    }
+    
+}
